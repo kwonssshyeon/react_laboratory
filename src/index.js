3ddb97acc734020825ecc587dc8612c832fa7d21
kwonssshyeon <kwonsh4396@gmail.com>
2024-04-07 16:37:36 +0900

    Element 생성 확인

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Library from './jsx_test/Library';
+import Clock from './element_test/Clock';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// root.render(
+//   <React.StrictMode>
+//     <Clock />
+//   </React.StrictMode>
+// );
+
+setInterval(() =>{
+  root.render(
+    <React.StrictMode>
+      <Clock />
+    </React.StrictMode>,
+  );
+},1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
